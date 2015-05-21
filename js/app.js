77cdaf79866db329482839085b294bdf4f7589d7
Asad Shahabuddin <asad.shahabuddin@gmail.com>
2015-05-20 20:51:47 -0400

    Added address search and Uber price querying

--- js/app.js
+++ js/app.js
@@ -9,29 +9,84 @@ var app = angular.module("UberSPApp", []);
 console.log("%c>Uber Surge Pricing application",
             "color: navy; font-family: Courier New; font-weight: bold");
 
-app.controller("UberSPController", function($scope, $http)
+app.controller("UberSPController", function($q, $scope, $http)
 {
-    /* =================== */
-    /* GOOGLE MAPS : BEGIN */
-    /* =================== */
-
+    /* Google Maps constants */
     var map;
+    var orgnLatLng;
+    var orgnCoords;
+    var destCoords;
+    /* Uber API Constants */
+    var uberClientId    = "EVYAt6HcmlxXyvp3m7FX4k5claG6Bqa",
+        uberServerToken = "i62YyuNa-budKDytw1Le6HTQzt-p17kwG8ub0B72";
 
-    function initalize()
+    /* ============================= */
+    /* GOOGLE MAPS FUNCTIONS : BEGIN */
+    /* ============================= */
+
+    /* Initialize Google Maps */
+    function initGoogleMaps()
     {
         var markers = [];
         var mapOptions = {
-            center   : new google.maps.LatLng(42.3344028, -71.1009781),
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
-            zoom     : 15
+            mapTypeControl          : true,
+            mapTypeControlOptions   : {
+                style   : google.maps.MapTypeControlStyle.HORIZONTAL_BAR
+            },
+            mapTypeId               : google.maps.MapTypeId.ROADMAP,
+            streetViewControl       : true,
+            streetViewControlOptions: {
+                position: google.maps.ControlPosition.LEFT_CENTER
+            },
+            zoom                    : 15,
+            zoomControl             : true,
+            zoomControlOptions      : {
+                position: google.maps.ControlPosition.LEFT_CENTER,
+                style   : google.maps.ZoomControlStyle.LARGE
+            }
         };
         map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
-        /* Create a search field and link it to the UI element */
+        /* Try HTML5 geolocation */
+        if(navigator.geolocation)
+        {
+            navigator.geolocation.getCurrentPosition(function(pos)
+            {
+                orgnCoords = pos.coords;
+                orgnLatLng = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+                var infoWindow = new google.maps.InfoWindow(
+                {
+                    map     : map,
+                    position: orgnLatLng,
+                    content : "You are here."
+                });
+
+                console.log("%c   [echo] Current position is (" + pos.coords.latitude +
+                                            ", " + pos.coords.longitude + ")",
+                                            "font-family: Courier New;");
+                map.setCenter(orgnLatLng);
+                reverseGeocode(orgnLatLng, document.getElementById("orgn-addr"));  /* Set the origin address field */
+            },
+            function()
+            {
+                handleNoGeolocation(true);
+            });
+        }
+        else
+        {
+            /* Browser does not support geolocation */
+            handleNoGeolocation(false);
+        }
+
+        /* Create search fields and link them to the UI element */
         var input = /** @type {HTMLInputElement} */(document.getElementById("pac-input"));
+        var orgnAddrInput = /** @type {HTMLInputElement} */(document.getElementById("orgn-addr"));
+        var destAddrInput = /** @type {HTMLInputElement} */(document.getElementById("dest-addr"));
         map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
         var searchBox = new google.maps.places.SearchBox(/** @type {HTMLInputElement} */(input));
+        var orgnAddrBox = new google.maps.places.SearchBox(/** @type {HTMLInputElement} */(orgnAddrInput));
+        var destAddrBox = new google.maps.places.SearchBox(/** @type {HTMLInputElement} */(destAddrInput));
 
         /* Listen for the event fired when the user selects an item from the
         pick list. Retrieve the matching places for that item */
@@ -76,59 +131,118 @@ app.controller("UberSPController", function($scope, $http)
             map.fitBounds(bounds);
         });
 
-        /* Bias the search box results towards places that are within the bounds
+        google.maps.event.addListener(destAddrBox, "places_changed", function()
+        {
+            var places = destAddrBox.getPlaces();
+            if(places.length == 0)
+            {
+                return;
+            }
+            destCoords = places[0].geometry.location;
+        });
+
+
+        /* Bias the search boxes' results towards places that are within the bounds
         of the current map's viewport */
         google.maps.event.addListener(map, "bounds_changed", function()
         {
-            var bounds = map.getBounds();
-            searchBox.setBounds(bounds);
+            searchBox.setBounds(map.getBounds());
+            orgnAddrBox.setBounds(map.getBounds());
+            destAddrBox.setBounds(map.getBounds());
         });
     };
 
-    google.maps.event.addDomListener(window, "load", initalize);
+    /* Handle the lack of geolocation support */
+    function handleNoGeolocation(errFlag)
+    {
+        if(errFlag)
+        {
+            var content = "ERROR: The Geolocation service failed.";
+        }
+        else
+        {
+            var content = "ERROR: Your browser does not support Geolocation.";
+        }
 
-    /* ================= */
-    /* GOOGLE MAPS : END */
-    /* ================= */
+        var mapOptions = {
+            map     : map,
+            position: new google.maps.LatLng(42.3344028, -71.1009781),
+            content : content
+        };
+        var infoWindow = new google.maps.InfoWindow(mapOptions);
+        map.setCenter(macpOptions.position);
+    };
 
-    /* ============ */
-    /* UBER : BEGIN */
-    /* ============ */
+    /* Reverse geocoding to a formatted address */
+    function reverseGeocode(latlng, elem)
+    {
+        var geocoder = new google.maps.Geocoder();
 
-    /* Uber API Constants */
-    // var uberClientId = "EVYAt6HcmlxXyvp3m7FX4k5claG6Bqa",
-    //     uberServerToken = "i62YyuNa-budKDytw1Le6HTQzt-p17kwG8ub0B72";
+        geocoder.geocode({"latLng": latlng}, function(res, status)
+        {
+            if(status = google.maps.GeocoderStatus.OK)
+            {
+                if(res[0])
+                {
+                    console.log("%cCurrent address>",
+                                "font-family: Courier New; font-weight: bold;");
+                    console.log("%c" + res[0].formatted_address,
+                                "font-family: Courier New; font-weight: bold;");
+                    elem.value = res[0].formatted_address;
+                }
+            }
+        });
+    };
+
+    /* =========================== */
+    /* GOOGLE MAPS FUNCTIONS : END */
+    /* =========================== */
+
+    /* ====================== */
+    /* UBER FUNCTIONS : BEGIN */
+    /* ====================== */
 
     /* Create variables to store latitude and longitude */
-    // var userLatitude   = 42.3344028,
-    //     userLongitude  = -71.1009781,
-    //     partyLatitude  = 42.3508452,
-    //     partyLongitude = -71.1481954;
-
     /*
-    $.ajax(
-    {
-        url: "https://api.uber.com/v1/estimates/price",
-        headers:
-        {
-            Authorization: "Token "  + uberServerToken
-        },
-        data:
-        {
-            start_latitude: userLatitude,
-            start_longitude: userLongitude,
-            end_latitude: partyLatitude,
-            end_longitude: partyLongitude
-        },
-        success: function(res)
-        {
-            console.log(res);
-        }
-    });
+    var userLatitude  = originLat,
+        userLongitude = originLong,
+        destLatitude  = 42.3508452,
+        destLongitude = -71.1481954;
     */
 
-    /* ========== */
-    /* UBER : END */
-    /* ========== */
+    function queryUber(orgnLat, orgnLng, destLat, destLng)
+    {
+        $.ajax(
+        {
+            url: "https://api.uber.com/v1/estimates/price",
+            headers:
+            {
+                Authorization: "Token "  + uberServerToken
+            },
+            data:
+            {
+                start_latitude : orgnLat,
+                start_longitude: orgnLng,
+                end_latitude   : destLat,
+                end_longitude  : destLng
+            },
+            success: function(res)
+            {
+                console.log(res);
+            }
+        });
+    };
+
+    /* ==================== */
+    /* UBER FUNCTIONS : END */
+    /* ==================== */
+
+    $scope.findUber = function()
+    {
+        queryUber(orgnCoords.latitude, orgnCoords.longitude, destCoords.A, destCoords.F);
+    };
+
+    /* Main */
+    google.maps.event.addDomListener(window, "load", initGoogleMaps);
 });
 /* End of app.js */
